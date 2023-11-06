dce6d6982d89cda1dda5187a25736f418c509bec
Alfonso Yeste <68972892+alfonso-yeste@users.noreply.github.com>
2023-11-06 23:27:19 +0100

    Update NewSessionVsReturnSessions.sql

--- NewSessionVsReturnSessions.sql
+++ NewSessionVsReturnSessions.sql
@@ -1,9 +1,9 @@
 WITH max_sessions as (
 SELECT max(event_params.value.int_value) as session_number
 FROM
-  `xuanlanyoga.analytics_322908039.events_*`, UNNEST (event_params) as event_params
+  `XXXXX.events_*`, UNNEST (event_params) as event_params
 WHERE
- _TABLE_SUFFIX between '20230828' and '20230909'
+ _TABLE_SUFFIX between 'XXXX' and 'XXXX'
   AND event_params.key = "ga_session_number"
   group by user_pseudo_id)
 select SUM(CASE when max_sessions.session_number = 1 then 1 else 0 end) as one_session,
