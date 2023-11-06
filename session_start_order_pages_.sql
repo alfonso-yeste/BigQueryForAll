e7f221cc298bbc5c8a0e6c7f6d39791ff3fa468f
Alfonso Yeste <68972892+alfonso-yeste@users.noreply.github.com>
2023-11-06 23:27:57 +0100

    Update session_start_order_pages_.sql

--- session_start_order_pages_.sql
+++ session_start_order_pages_.sql
@@ -3,11 +3,11 @@ event_date,
  event_name,
   event_params.value.string_value,
 FROM
-  `xuanlanyoga.analytics_322908039.events_*`,
+  `XXXXX.events_*`,
   UNNEST(event_params) as event_params
 WHERE
   event_name = 'session_start' AND
-  _TABLE_SUFFIX between '20230713' and '20230713' AND
+  _TABLE_SUFFIX between 'XXXX' and 'XXXX' AND
   event_params.key = 'page_location' and event_params.value.string_value like '%order%'
 
   ;
