f5ac035c2a1afb09f87eca112bc6078ec763c20b
Alfonso Yeste <68972892+alfonso-yeste@users.noreply.github.com>
2023-11-06 23:26:25 +0100

    Update MenuInteractionAnalysis.sql

--- MenuInteractionAnalysis.sql
+++ MenuInteractionAnalysis.sql
@@ -4,9 +4,9 @@ FROM `XXXX.events_*`
 WHERE  _TABLE_SUFFIX BETWEEN 'XXXX' AND 'XXX'
 )
 SELECT event_params.value.string_value, count(*), count(distinct user_pseudo_id) as unique_users
-FROM `bellaaurora.analytics_348556202.events_*` as event, users, UNNEST(event_params) as event_params
+FROM `XXXX.events_*` as event, users, UNNEST(event_params) as event_params
 WHERE 
-_TABLE_SUFFIX BETWEEN '20230210' AND '20230430' AND 
+_TABLE_SUFFIX BETWEEN 'XXXX' AND 'XXXX' AND 
 event_name = 'menu' AND 
 event_params.key = 'action'
 group by event_params.value.string_value
