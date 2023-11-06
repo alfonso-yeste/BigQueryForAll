5c8eb95b250222e15e4cb46df1df8a527e7da991
Alfonso Yeste <68972892+alfonso-yeste@users.noreply.github.com>
2023-11-06 23:24:18 +0100

    Create PercentageNewSessions.sql

---
+++ PercentageNewSessions.sql
@@ -0,0 +1,10 @@
+WITH max_sessions as (
+SELECT max(event_params.value.int_value) as session_number
+FROM
+  `XXXXX.events_*`, UNNEST (event_params) as event_params
+WHERE
+ _TABLE_SUFFIX between 'XXXX' and 'XXXX'
+  AND event_params.key = "ga_session_number"
+  group by user_pseudo_id)
+select SUM(CASE when max_sessions.session_number > 1 then 1 else 0 end)/SUM(CASE when max_sessions.session_number = 1 then 1 else 0 end)*100 as total
+from max_sessions;
