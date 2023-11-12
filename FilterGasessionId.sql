47e96807b481f164962bcbecef4e89074aa3248b
Alfonso Yeste <68972892+alfonso-yeste@users.noreply.github.com>
2023-11-12 10:08:49 +0100

    Update FilterGasessionId.sql

--- FilterGasessionId.sql
+++ FilterGasessionId.sql
@@ -1,3 +1,10 @@
 SELECT event_params.* 
 FROM `vidalvidal.analytics_267182121.events_20231014*` , UNNEST (event_params) as event_params
 WHERE event_name = 'purchase' AND event_params.key = 'ga_session_id'
+
+
+--You can do this directly, regardless of whether you include 'key=' or 'event_params=' in the query, among other parameters
+
+SELECT event_params.* 
+FROM `vidalvidal.analytics_267182121.events_20231014*` , UNNEST (event_params) as event_params
+WHERE event_name = 'purchase' AND key = 'ga_session_id'
