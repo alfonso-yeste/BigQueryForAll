7d08c7bb80ec45f28ddf91ec07ac25b2d9733399
Alfonso Yeste <68972892+alfonso-yeste@users.noreply.github.com>
2023-11-12 11:06:40 +0100

    Update FirstExampleWith.sql

--- FirstExampleWith.sql
+++ FirstExampleWith.sql
@@ -1,4 +1,4 @@
-WITH t_sessions as (SELECT  event_date, user_pseudo_id, event_timestamp, device.category, key, value.string_value as sesiones
+WITH t_sessions as (SELECT  event_date, user_pseudo_id, event_timestamp, device.category, key, value.key_value as sesiones
 FROM `vidalvidal.analytics_267182121.events_*`,
 UNNEST (event_params) as event_params
 WHERE event_name= 'purchase' AND _TABLE_SUFFIX BETWEEN '20231015' AND '20231016')
