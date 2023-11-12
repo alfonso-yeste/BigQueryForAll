3b399c76b3c067994ff93d5028c7895cca3562d8
Alfonso Yeste <68972892+alfonso-yeste@users.noreply.github.com>
2023-11-12 12:41:09 +0100

    Update 5WithUnion3Tables.sql

--- 5WithUnion3Tables.sql
+++ 5WithUnion3Tables.sql
@@ -2,7 +2,7 @@ WITH t_sesiones as (SELECT event_date, event_timestamp ,user_pseudo_id, value.in
 FROM `vidalvidal.analytics_267182121.events_20231015` , UNNEST(event_params) as event_params 
 WHERE event_name = "purchase" and key = 'ga_session_id'),
 
-t_page_title as (SELECT event_date, event_timestamp ,user_pseudo_id, value.int_value as page_title
+t_page_title as (SELECT event_date, event_timestamp ,user_pseudo_id, value.string_value as page_title
 FROM `vidalvidal.analytics_267182121.events_20231015` , UNNEST(event_params) as event_params 
 WHERE event_name = "purchase" and key = 'page_title'),
 
